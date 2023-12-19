d10155eec509400efdfc6e6df2191308ef0c4cb5
Muhammad Rizki Rijal <arkinthesky.69@gmail.com>
2023-12-19 09:29:14 +0800

    try to track add to cart

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 const { createApp, ref } = Vue
-createApp({
+
+const app = createApp({
   setup() {
     const message = ref('Hello vue!')
 
     return { message }
-  }
-}).mount('#app')
+  },
+});
+
+const ProductList = app.component('ProductList', {
+  setup() {
+    const products = ref([
+      { id: 123, title: 'Product A', price: 8000 },
+      { id: 456, title: 'Product B', price: 200 }
+    ])
+    const cart = ref([])
+
+    function addToCart(product) {
+      cart.value.push(product)
+    }
+
+    return { products, cart, addToCart }
+  },
+  template: `
+  <div>
+    <p>Cart ({{ cart.length }})</p>
+    <h2>Products</h2>
+    <div style="display: flex; gap: 1rem">
+      <div style="padding: 0.5rem;" v-for="product in products" :key="product.id">
+        <p>{{product.title}}</p>
+        <p>Rp. {{product.price}}</p>
+
+        <button class="add-to-cart" @click="addToCart(product)">Add To Cart</button>
+      </div>
+    </div>
+
+    <button class="TR-checkout">Checkout</button>
+    <button class="TR-purchase">Purchase</button>
+  </div>
+  `
+})
+
+app.mount('#app')
